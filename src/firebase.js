f196ad2d8b023798ed1d08048ba18ea0ce045a5c
Alan Binu <alanbinu5@gmail.com>
2022-01-30 13:12:43 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -2,13 +2,7 @@ import firebase from "firebase";
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 //GET Below Settings from Firebase > Project Overview > Settings > General > Your apps > Firebase SDK snippet > Config
 const firebaseConfig = {
-  apiKey: "AIzaSyDC_O7A0m2570Qgi_lgetjOuWOzD-YG2uw",
-  authDomain: "alanbinu-discord.firebaseapp.com",
-  projectId: "alanbinu-discord",
-  storageBucket: "alanbinu-discord.appspot.com",
-  messagingSenderId: "18335583725",
-  appId: "1:18335583725:web:e0ae17ac99468f9390abe4",
-  measurementId: "G-E01KDMM4CE",
+  //your app config
 };
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
